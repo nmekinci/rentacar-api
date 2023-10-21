e6eac530ed79be65885e772cdfdcf362a5efa02c
Must <nm.ekinci@gmail.com>
2023-10-21 22:30:26 +0200

    reservation model

---
+++ src/models/reservation.js
@@ -0,0 +1,29 @@
+"use strict"
+
+const {Schema, model} = require('mongoose')
+
+const ReservationSchema = new Schema({
+    creatorId: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    },
+    carId:{
+        type: Schema.Types.ObjectId,
+        ref: 'Car',
+        required: true,
+    },
+    startDate:{
+        type:Date,
+        required: true,
+        // default: new Date()
+    },
+    endDate:{
+        type:Date,
+        required: true,
+    },
+    totalPrice: {
+        type: Number,
+        default: 0
+    }
+},{collection: 'reservations', timestamps:true})
