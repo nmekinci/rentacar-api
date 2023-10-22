d3427d820a708b4fe4a02aeb8efa5abb79f957f0
Must <nm.ekinci@gmail.com>
2023-10-22 15:10:27 +0200

    index.js

---
+++ index.js
@@ -0,0 +1,37 @@
+"use strict"
+// Rent A Car API
+
+// mkdir logs
+// npm init -y
+// npm i express dotenv mongoose express-async-errors
+// npm i jsonwebtoken morgan
+
+const express = require('express')
+const app = express()
+
+// Modules
+
+require('dotenv').config()
+const PORT = process.env?.PORT || 8000
+
+require('express-async-errors')
+
+//configurations
+
+
+
+//Middlewares
+
+app.use(express.json())
+
+
+//homepath
+app.all('/', (req,res) => {
+    res.send({
+        error:false,
+        message: 'Rent A Car API',
+        user: req.user
+    })
+})
+//server 
+app.listen(PORT, ()=> console.log('http://127.0.0.1:' + PORT) )
