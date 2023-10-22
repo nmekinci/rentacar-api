09b200c318f8d06aba60972abfa2b4b377954947
Must <nm.ekinci@gmail.com>
2023-10-22 22:35:35 +0200

    end of day

--- src/middlewares/permissions.js
+++ src/middlewares/permissions.js
@@ -1,13 +1,28 @@
 "use strict"
 
+// module.exports = {
+//     isLogin: (req,res,next) => {
+//         req.user ? next() : (res.errorStatusCode = 403, throw new Error('NoPermission: You must login'));
+//     },
+//     isStaff : (req,res,next) => {
+//         req.user.isStaff ? next() : (res.errorStatusCode = 403, throw new Error('NoPermission: You must be a staff'));
+//     },
+//     isAdmin : (req,res,next) => {
+//         req.user.isAdmin ? next() : (res.errorStatusCode = 403, throw new Error('NoPermission: You must be an admin'));
+//     }
+}
+
 module.exports = {
     isLogin: (req,res,next) => {
-        req.user ? next() : (res.errorStatusCode = 403, throw new Error('NoPermission: You must login'))
+        if(req.user){ next()}else {res.errorStatusCode = 403
+             throw new Error('NoPermission: You must login')}
     },
     isStaff : (req,res,next) => {
-        req.user.isStaff ? next() : (res.errorStatusCode = 403, throw new Error('NoPermission: You must be a staff'))
+       if (req.user.isStaff) { next()} else {res.errorStatusCode = 403
+         throw new Error('NoPermission: You must be a staff')}
     },
     isAdmin : (req,res,next) => {
-        req.user.isAdmin ? next() : (res.errorStatusCode = 403, throw new Error('NoPermission: You must be an admin'))
+        if (req.user.isAdmin) { next()} else {res.errorStatusCode = 403
+             throw new Error('NoPermission: You must be an admin')}
     }
 }
