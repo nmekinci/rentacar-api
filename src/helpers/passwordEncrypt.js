43aab131bb96f02572d8e50221aa0901bef42386
Must <nm.ekinci@gmail.com>
2023-10-23 12:19:30 +0200

    passwordEncrypt.js

---
+++ src/helpers/passwordEncrypt.js
@@ -0,0 +1,13 @@
+"use strict"
+
+// password encrypt
+
+const crypto = require('node:crypto')
+keycode = process.env.SECRET_KEY,
+loopCount = 8000,
+charCount = 32,
+encType = 'sha512'
+
+module.exports = (password) => {
+    return crypto.pbkdf2Sync(password,keycode,loopCount,charCount,encType).toString('hex')
+}
